9c1228b85473b05a7dd140b6dd923b72ebda0987
telusashish <ashish.sharma6@telus.com>
2025-08-21 17:28:37 -0600

    fixed moduleNotFound Error on warpstream

--- components/warpstreamagents/warpstream_agents.py
+++
@@ -1,6 +0,0 @@
-# Top level Component to orchestrate all WarpStream Agent resources
-import pulumi
-from pulumi_gcp import storage as gcp_storage
-from pulumi_gcp import secretmanager as gcp_secretmanager
-from pulumi_gcp import projects as gcp_projects
-from pulumi_gcp import serviceaccount as gcp_serviceaccount
