6767ef14f4c35c60a455ffc77f71af26bf1aeea4
telusashish <ashish.sharma6@telus.com>
2025-08-21 22:03:48 -0600

    Fixed kubeconfig_gke_exec in __init__ file

--- components/__init__.py
+++ components/__init__.py
@@ -1,8 +1,8 @@
-from kubeconfig.kubeconfig import kubeconfig_gcp_user 
+from kubeconfig.kubeconfig import kubeconfig_gke_exec
 from gkecluster import GKECluster, GKEClusterArgs
 from warpstreamagents import WarpstreamCluster, WarpstreamClusterArgs
 
-__all__ = ['GKECluster', 'GKEClusterArgs', 'kubeconfig_gcp_user', 'WarpstreamCluster', 'WarpstreamClusterArgs']
+__all__ = ['GKECluster', 'GKEClusterArgs', 'kubeconfig_gke_exec', 'WarpstreamCluster', 'WarpstreamClusterArgs']
 
 
 
--- components/kubeconfig/__init__.py
+++ components/kubeconfig/__init__.py
@@ -1,1 +1,1 @@
-from .kubeconfig import kubeconfig_gcp_user, kubeconfig_bearer_token, make_k8s_provider
+from .kubeconfig import kubeconfig_gke_exec
